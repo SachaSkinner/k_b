1e70f61238e473148127e6a2b95acabb001cc28f
Alexandra Skinner <aleksandrayandovka@gmail.com>
2020-05-25 14:18:47 -0500

    alighed the dropdown- colors and padding

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-$('.dropdown-content').hide();
-$('.dropbtn').on('click', function() {
+// $('.dropdown-content1').hide();
+// $('.dropbtn1').on('click', function() {
 
-    $('.dropdown-content').show();
-})
+//     $('.dropdown-content1').css('display', 'block');
+// })
+// $('.dropdown-content1').on('mouseleave', function() {
+//     $('.dropdown-content1').css('display', 'none');
+// })
