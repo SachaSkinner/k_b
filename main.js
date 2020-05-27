059bee2d37840ea267d80fc7d154d2d5f11f6837
Alexandra Skinner <aleksandrayandovka@gmail.com>
2020-05-27 17:21:42 -0500

    added fadeOUt and fadeIn for a logo with jquery

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-// $('.dropdown-content1').hide();
-// $('.dropbtn1').on('click', function() {
+$('#logo').on('click', function() {
 
-//     $('.dropdown-content1').css('display', 'block');
-// })
-// $('.dropdown-content1').on('mouseleave', function() {
-//     $('.dropdown-content1').css('display', 'none');
-// })
+    $('#logo').fadeOut().fadeIn();
+})
